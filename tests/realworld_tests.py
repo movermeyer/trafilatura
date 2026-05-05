ccb204849c31d83716e756bae3283950302d64d6
Sprite <noreply@sprites.dev>
2026-05-05 21:10:18 +0000

    Remove incorrect test assertion
    This is a test about link formatting anyway. It was just there because it was convenient
    
    Added in https://github.com/adbar/trafilatura/pull/528

--- tests/realworld_tests.py
+++ tests/realworld_tests.py
@@ -513,9 +513,6 @@ def test_extract(xmloutput, formatting):
 def test_extract_links_formatting():
     result = load_mock_page('http://www.pcgamer.com/2012/08/09/skyrim-part-1/', formatting=True, links=True)
     assert 'In [Skyrim](https://www.pcgamer.com/best-skyrim-mods/), a mage' in result
-    # the original has the space at the end of the em tag
-    assert "*Legends *don't destroy *houses*," in result
-
 
 def test_pages():
     '''Test on real web pages'''
